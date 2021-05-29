3a5275700bbcaccc66080341862cc3de807e7171
Julius Schmid <55657402+jus2008@users.noreply.github.com>
2021-05-29 13:20:19 +0200

    Taschenrechner

--- Taschenrechner/ViewController.swift
+++ Taschenrechner/ViewController.swift
@@ -8,11 +8,89 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var resultLabel: UILabel!
+ 
+    var userIsInTheMiddleOfTyping : Bool = false
+    var decimalusing : Bool = false
+    var firstNumber : Double = 0.0
+    var secondNumber : Double = 0.0
+    var result : Double = 0.0
+    var choosingOperator : String = ""
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+
+    
+    
+    
+    
+  
+    @IBAction func nummberButton_Tapped(_ sender: UIButton) {
+    
+        var key = sender.currentTitle!
+        if userIsInTheMiddleOfTyping == true {
+            
+            if decimalusing == true && key == "." {
+                
+                key = "" }
+            else {
+                resultLabel.text = key
+                userIsInTheMiddleOfTyping = true
+             }
+            
+            if key == "." {
+                decimalusing = true
+                
+            }
+            
+        }
+    
+        resultLabel.text = resultLabel.text! + key
+    
+    }
+    
+    
+    
+    @IBAction func equalButton_Tapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    @IBAction func clearButton_Tapped(_ sender: UIButton) {
+        firstNumber = 0
+        secondNumber = 0
+        result = 0
+        choosingOperator = ""
+        resultLabel.text = "0"
+        decimalusing = false
+    
+    }
+    
+    
+    
+    @IBAction func operationButton_Tapped(_ sender: UIButton) {
+    }
+    
+
+
+
+
+
+
+
+
+
+
 
 
 }
